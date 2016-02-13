134d4d7134054142b0176c1d6ec50dafb2341d57
Daniel Kochmański <daniel@turtleware.eu>
2016-02-13 11:17:08 +0100

    fix: refine run-ecl*.sh scripts. Add bytecmp script.

--- run-ecl-bytecmp.sh
+++ run-ecl-bytecmp.sh
@@ -4,12 +4,14 @@ ECL=ecl
 
 make clean optimize-files
 ${ECL} <<EOF
+(ext:install-bytecodes-compiler)
 (load "sysdep/setup-ecl.lisp")
 (load "do-compilation-script.lisp")
 (quit)
 EOF
 
 ${ECL} <<EOF
+(ext:install-bytecodes-compiler)
 (load "sysdep/setup-ecl.lisp")
 (load "do-execute-script.lisp")
 (quit)
